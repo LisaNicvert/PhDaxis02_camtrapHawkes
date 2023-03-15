ceb919fc45bdfad175db1423bb7c7b757b634f29
Lisa Nicvert <lisa.nicvert@univ-lyon1.fr>
2023-03-15 14:41:40 +0100

    Corrected error in bash syntax

--- analyses/02_simulation_interevent_times/01_launch_jobs.sh
+++ analyses/02_simulation_interevent_times/01_launch_jobs.sh
@@ -13,7 +13,7 @@ nrep=30
 # --- Run the script locally or on a computing cluster?
 run_locally=false
 
-if [ ! $run_locally ]; then
+if ! $run_locally ; then
   # ========== navigate to directory ==========
   cd /beegfs/home/lnicvert/camtrapHawkes/
   
@@ -33,7 +33,7 @@ if [ ! $run_locally ]; then
 fi
 
 # ========== Launch jobs ==========
-i=1
+i=0
 # --- Iterate through strength
 for strength in ${strengthlist[@]}
 do
@@ -44,10 +44,12 @@ do
 		for rep in $(seq 1 1 $nrep)
 		do
 			i=$((i+1))
-			# Used on the cluster
-			if [ ! $run_locally ]; then
+			
+			if ! $run_locally ; then
+			  # Used on the cluster
 			  sbatch --job-name str$strength-tmax$tmax-rep$rep-i$i --output outputs/02_simulation_interevent_times/sbatch/output/output_i$i-str$strength-tmax$tmax-rep$rep.out --error outputs/02_simulation_interevent_times/sbatch/error/error_i$i-str$strength-tmax$tmax-rep$rep.out --time=$time --nodes=1 --cpus-per-task=4 --mem=1G /beegfs/home/lnicvert/camtrapHawkes/analyses/02_simulation_interevent_times/01-1_compute_inter_event_times.slurm $strength $tmax $rep $i $run_locally
 			else
+			  # Used locally
 			  ./01-1_compute_inter_event_times.slurm $strength $tmax $rep $i $run_locally
 			fi
 		done 
